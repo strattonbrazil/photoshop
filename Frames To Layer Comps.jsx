dc1690dce94e121bfa50a2da103368b5f1f17ef1
Josh Stratton <strattonbrazil@gmail.com>
2015-02-03 20:22:31 -0800

    initial commit

---
+++ Frames To Layer Comps.jsx
@@ -0,0 +1,37 @@
+﻿// written by Josh Stratton (2013) - strattonbrazil@gmail.com
+
+function getFrameCount() {
+    for (var i = 1; i < 10000; i++) {
+        try {
+            jumpToFrame(i);
+        } catch (err) {
+            return i-1;
+        }
+    }
+}
+
+function jumpToFrame(frameNumber) {
+    var desc = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putIndex( stringIDToTypeID( "animationFrameClass" ), frameNumber );
+    desc.putReference( charIDToTypeID( "null" ), ref1 );
+    executeAction( charIDToTypeID( "slct" ), desc, DialogModes.NO );
+ }
+
+// save off layer comp names
+var layerCompNames = new Array();
+for (var i = 0; i < app.activeDocument.layerComps.length; i++) {
+    layerCompNames.push(app.activeDocument.layerComps[i].name);
+}
+app.activeDocument.layerComps.removeAll();
+
+var numFrames = getFrameCount();
+
+for (var i = 0; i < numFrames; i++) {
+    jumpToFrame(i+1);
+    layerCompName = "layerComp" + (i + 1);
+    if (i < layerCompNames.length) {
+        layerCompName = layerCompNames[i];
+    }
+    app.activeDocument.layerComps.add(layerCompName, "", true, true, true);
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # photoshop
-photoshop scripts for artists
+
+A collection of photoshop scripts written for my Photoshop-using buddies.  I'm not a photoshop guy, but I'm told these are useful to some people.  
+
+# INSTALL
+
+If you're not a programmer, but just want to use these scripts in photoshop...
+
+* in the above list of files click on the file you want
+* click on the "Raw" button to see just the file (no github controls)
+* right-click and select "Save as..." or go to the main menu and select "Save page as..." or hit Ctrl+s
+* save the file in your photoshop scripts directory (usually "Presets/Scripts")
+** the file name is the command you see in the menu
+
+# Frames To Layer Comps
+
+A talented friend of mine needed a tool for turning a bunch of frames from the animation timeline into layer comps.  
